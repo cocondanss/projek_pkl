fbb241765ea0ccb32f23b3c8dc1be490a5d6eac6
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 11:18:09 +0700

    login pt 2

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if(isset($_POST['login'])){
 
      if($hitung>0){
         $_SESSION['log'] = 'true';
-        header('location:login.php');
+        header('location:produk.php');
      } else {
         header('location:login.php');
      };
