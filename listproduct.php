d9c878ebf2ed840a9c03b1b136fa7897625da462
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 09:52:13 +0700

    ubah posisi gembok

--- listproduct.php
+++ listproduct.php
@@ -14,7 +14,6 @@
             <rect x="3" y="11" width="18" height="10" rx="2" fill="#fff" />
             <path d="M7 11V7a5 5 0 0 1 10 0v4" />
         </svg>
-        Kembali
     </button>
     </a>
     <div class="container-index">
