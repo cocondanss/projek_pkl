4f0e4a584f9891da02c92356e7cafc5f7111a111
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 11:27:24 +0700

    ubah menjadi index

---
+++ user.php
@@ -0,0 +1,217 @@
+<?php
+require 'function.php';
+require 'cek.php';
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>User</title>
+        <link href="css/style.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php" style="color: white;">Daclen</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                        <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                User
+                            </a>
+                            <a class="nav-link" href="produk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Produk
+                            </a>
+                            <a class="nav-link" href="transaksi.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Transaksi
+                            </a>
+                            <a class="nav-link" href="voucher.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Voucher
+                            </a>
+                            <a class="nav-link" href="logout.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Logout
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">User</h1>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
+                                    Tambah User
+                             </button>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Nama User</th>
+                                                <th>Status</th>
+                                                <th>Umur</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+                                            $ambilsemuadatauser = mysqli_query($conn, "select * from user");
+                                            $i = 1;
+                                            while($data=mysqli_fetch_array($ambilsemuadatauser)){
+                                                $namauser = $data['namauser'];
+                                                $status = $data['status'];
+                                                $umur = $data['umur'];
+                                                $idu = $data['iduser'];
+
+
+                                            ?>
+
+                                            <tr>
+                                                <td><?=$i++;?></td>
+                                                <td><?=$namauser;?></td>
+                                                <td><?=$status;?></td>
+                                                <td><?=$umur;?></td>
+                                                <td>
+                                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?=$idu;?>">
+                                                        Edit
+                                                </button>
+                                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$idu;?>">
+                                                        Delete
+                                                </button>
+                                                </td>
+                                            </tr>
+
+
+                                            <!-- Edit Modal -->
+                                            <div class="modal fade" id="edit<?=$idu;?>">
+                                                <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                
+                                                    <!-- Modal Header -->
+                                                    <div class="modal-header">
+                                                    <h4 class="modal-title">Edit User</h4>
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    </div>
+                                                    
+                                                    <!-- Modal body -->
+                                                    <form method="post">
+                                                    <div class="modal-body">
+                                                    <input type="text" name="namauser" value="<?=$namauser;?>" class="form-control" required><br>
+                                                    <input type="text" name="status" value="<?=$status;?>" class="form-control" required><br>
+                                                    <input type="number" name="umur" value="<?=$umur;?>" class="form-control" required><br>
+                                                    <input type="hidden" name="idu" value="<?=$idu?>">
+                                                    <button type="submit" class="btn btn-primary" name="updateuser">Submit</button><br>
+                                                    </div>
+                                                    </form>
+
+                                                    </div>
+                                                    </div>
+                                                </div>
+
+                                                <!-- Delete Modal -->
+                                            <div class="modal fade" id="delete<?=$idu;?>">
+                                                <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                
+                                                    <!-- Modal Header -->
+                                                    <div class="modal-header">
+                                                    <h4 class="modal-title">Hapus User?</h4>
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    </div>
+                                                    
+                                                    <!-- Modal body -->
+                                                    <form method="post">
+                                                    <div class="modal-body">
+                                                    Apakah Anda Yakin Ingin Menghapus <?=$namauser;?>?
+                                                    <input type="hidden" name="idu" value="<?=$idu?>">
+                                                    <br>
+                                                    <br>
+                                                    <button type="submit" class="btn btn-danger" name="hapususer">Hapus</button><br>
+                                                    </div>
+                                                    </form>
+
+                                                    </div>
+                                                    </div>
+                                                </div>
+
+
+                                            <?php
+                                            };
+
+                                            ?>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
+
+    <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Tambah User</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+         <form method="post">
+         <div class="modal-body">
+        <input type="text" name="namauser" placeholder="Nama User" class="form-control" required><br>
+        <input type="text" name="status" placeholder="Status User" class="form-control" required><br>
+        <input type="number" name="umur" placeholder="Umur User" class="form-control" required><br>
+        <button type="submit" class="btn btn-primary" name="TambahUser">Submit</button><br>
+        </div>
+        </form>
+
+    </div>
+    </div>
+</div>
+</html>
