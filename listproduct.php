1ab1ec84b0cdb8ebd147d8e1806a133fa1b1f215
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 10:19:56 +0700

    ubah

--- listproduct.php
+++ listproduct.php
@@ -15,7 +15,6 @@
             <rect x="3" y="11" width="18" height="10" rx="2" />
             <path d="M7 11V7a5 5 0 0 1 10 0v4" />
         </svg>
-    </a>
 </button>
     </a>
     <div class="container-index">
