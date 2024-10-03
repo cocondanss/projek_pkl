d1083c0e79e9c68161f31d0a20244832f0577731
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 11:06:32 +0700

    otomatis logout pt2

--- cek.php
+++ cek.php
@@ -4,3 +4,9 @@ if(isset($_SESSION['log'])){
 } else {
     header('location:listproduct.php');
 }
+
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: listproduct.php');
+    exit;
+}
