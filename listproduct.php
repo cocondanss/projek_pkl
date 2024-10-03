6aa39fb8e002dd8af2d6559b46ee67bb760c3dca
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 09:53:24 +0700

    undo

--- listproduct.php
+++ listproduct.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <a href="login.php" class="button"><button>
+    <a href="login.php"><button>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <rect x="3" y="11" width="18" height="10" rx="2" fill="#fff" />
             <path d="M7 11V7a5 5 0 0 1 10 0v4" />
