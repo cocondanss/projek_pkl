9643ed8ae1f2c68bd22d63b16535289e4c7daeb5
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 11:07:17 +0700

    ganti posisi

--- cek.php
+++ cek.php
@@ -1,11 +1,11 @@
 <?php
+session_start();
 if(isset($_SESSION['log'])){
 
 } else {
     header('location:listproduct.php');
 }
 
-session_start();
 if (!isset($_SESSION['user'])) {
     header('Location: listproduct.php');
     exit;
