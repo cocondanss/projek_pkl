477fc5229241cdfd247fe70ca162b0aee6b79d51
Idho <ridhoauliaakbar1@gmail.com>
2024-10-03 14:16:26 +0700

    posisi

--- listproduct.php
+++ listproduct.php
@@ -96,15 +96,15 @@
                     <button class="btn btn-number" onclick="appendNumber('1')">1</button>
                     <button class="btn btn-number" onclick="appendNumber('2')">2</button>
                     <button class="btn btn-number" onclick="appendNumber('3')">3</button>
-                    <button class="btn btn-backspace" onclick="backspace()"> <i class="fas fa-arrow-left"></i></button>
                     <button class="btn btn-number" onclick="appendNumber('4')">4</button>
                     <button class="btn btn-number" onclick="appendNumber('5')">5</button>
                     <button class="btn btn-number" onclick="appendNumber('6')">6</button>
-                    <button class="btn btn-enter" onclick="enter()"> <i class="fas fa-arrow-right"></i></button>
                     <button class="btn btn-number" onclick="appendNumber('7')">7</button>
                     <button class="btn btn-number" onclick="appendNumber('8')">8</button>
                     <button class="btn btn-number" onclick="appendNumber('9')">9</button>
+                    <button class="btn btn-backspace" onclick="backspace()"> <i class="fas fa-arrow-left"></i></button>
                     <button class="btn btn-number" onclick="appendNumber('0')">0</button>
+                    <button class="btn btn-enter" onclick="enter()"> <i class="fas fa-arrow-right"></i></button>
                 </div>
             </div>
         </div>
