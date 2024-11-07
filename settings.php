4b5d6116520633482b3de5f44adf98d23513274b
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-07 10:47:49 +0700

    d

--- settings.php
+++ settings.php
@@ -157,7 +157,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 </style>
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand bg-dark">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-light">
         <a class="navbar-brand" href="index.php" style="color: white;">Daclen</a>
         <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
     </nav>
