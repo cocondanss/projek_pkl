32fddb73c780e8a972f1f144addb6bda1ac78688
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-07 11:45:08 +0700

    p

--- voucher.php
+++ voucher.php
@@ -332,7 +332,7 @@ if (isset($_POST['TambahVoucherManual'])) {
         <script src="assets/demo/chart-area-demo.js"></script>
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <!-- <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script> -->
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/datatables-demo.js"></script>
     </div>
 
