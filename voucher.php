9732ead357c1d0b0d343213665d991b8e7e38be7
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-07 11:44:20 +0700

    p

--- voucher.php
+++ voucher.php
@@ -240,7 +240,7 @@ if (isset($_POST['TambahVoucherManual'])) {
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <table class="table table-bordered" id="" width="100%" cellspacing="0">
                                         <thead>
                                             <tr>
                                                 <th>No</th>
