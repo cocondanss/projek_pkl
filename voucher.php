ce86c2cccf65f2bc6fc17bb5d2a3c8185670f67f
hawa <hawanf1234@gmail.com>
2024-11-14 16:06:21 +0700

    gapapa

--- voucher.php
+++ voucher.php
@@ -258,72 +258,78 @@ function useVoucher($code) {
                                         </thead>
                                         <tbody>
                                         <?php
-                                                $ambilsemuadatavoucher = mysqli_query($conn, "SELECT * FROM vouchers2");
-                                                $i = 1;
-                                                while ($data = mysqli_fetch_array($ambilsemuadatavoucher)) {
-                                                    $code = $data['code'];
-                                                    $discount_amount = $data['discount_amount'];
-                                                    $is_free = $data['is_free'];
-                                                    $one_time_use = $data['one_time_use'];
-                                                    $id = $data['id'];
-                                                    $created_at = $data['created_at']; // UTC
-                                                    $used_at = $data['used_at']; // UTC
+// Ambil semua data voucher dari database
+$ambilsemuadatavoucher = mysqli_query($conn, "SELECT * FROM vouchers2");
 
-                                                    // Tentukan status berdasarkan used_at
-                                                    $status_used = !empty($used_at) ? "Sudah digunakan" : "Belum digunakan";
+if (!$ambilsemuadatavoucher) {
+    die("Query failed: " . mysqli_error($conn)); // Periksa apakah query berhasil
+}
 
-                                                    $isFreeDisplay = ($is_free == 1) ? "Ya" : "Tidak";
-                                                    $oneTimeUse = ($one_time_use == 1) ? "Ya" : "Tidak";
+$i = 1;
+while ($data = mysqli_fetch_array($ambilsemuadatavoucher)) {
+    $code = $data['code'];
+    $discount_amount = $data['discount_amount'];
+    $is_free = $data['is_free'];
+    $one_time_use = $data['one_time_use'];
+    $id = $data['id'];
+    $created_at = $data['created_at']; // UTC
+    $used_at = $data['used_at']; // UTC
 
-                                                    // Jika voucher gratis, set discount_amount menjadi 0
-                                                    if ($is_free == 1) {
-                                                        $discount_amount = 0;
-                                                    }
+    // Tentukan status berdasarkan used_at
+    $status_used = !empty($used_at) ? "Sudah digunakan" : "Belum digunakan";
 
-                                                    // Tentukan jenis voucher (diskon atau rupiah)
-                                                    $voucherType = ($discount_amount > 100) ? 'rupiah' : 'diskon';
-                                                ?>
-                                                    <tr>
-                                                        <td><?= $i++; ?></td>
-                                                        <td><?= htmlspecialchars($code); ?></td>
-                                                        <td>
-                                                            <?php if ($is_free == 1): ?>
-                                                                0
-                                                            <?php elseif ($voucherType == 'diskon'): ?>
-                                                                <?= htmlspecialchars($discount_amount) . '%' ?>
-                                                            <?php else: ?>
-                                                                <?= 'Rp ' . number_format($discount_amount, 0, ',', '.') ?>
-                                                            <?php endif; ?>
-                                                        </td>
-                                                        <td><?= htmlspecialchars($status_used); ?></td>
-                                                        <td><?= htmlspecialchars($isFreeDisplay); ?></td>
-                                                        <td><?= htmlspecialchars($oneTimeUse); ?></td>
-                                                        <td>
-                                                            <script>
-                                                                // Mengonversi waktu UTC ke waktu lokal untuk created_at
-                                                                var createdAtUTC = '<?= $created_at; ?>';
-                                                                var createdAtLocal = new Date(createdAtUTC + 'Z').toLocaleString('id-ID', { 
-                                                                    year: 'numeric', 
-                                                                    month: '2-digit', 
-                                                                    day: '2-digit', 
-                                                                    hour: '2-digit', 
-                                                                    minute: '2-digit', 
-                                                                    second: '2-digit', 
-                                                                    hour12: false // untuk format 24 jam
-                                                                });
+    $isFreeDisplay = ($is_free == 1) ? "Ya" : "Tidak";
+    $oneTimeUse = ($one_time_use == 1) ? "Ya" : "Tidak";
 
-                                                                // Menghapus bagian zona waktu dan mengganti '/' dengan '-'
-                                                                createdAtLocal = createdAtLocal.replace(/ GMT.*$/, ''); // Menghapus bagian GMT
-                                                                createdAtLocal = createdAtLocal.replace(/\//g, '-'); // Mengganti '/' dengan '-'
-                                                                document.write(createdAtLocal);
-                                                            </script>
-                                                        </td>
-                                                        <td><?= !empty($used_at) ? htmlspecialchars(date('d-m-Y H:i:s', strtotime($used_at))) : '-'; ?></td>
-                                                        <td><input type="checkbox" name="delete[]" value="<?= htmlspecialchars($id); ?>"></td>
-                                                    </tr>
-                                                <?php
-                                                }
-                                                ?>
+    // Jika voucher gratis, set discount_amount menjadi 0
+    if ($is_free == 1) {
+        $discount_amount = 0;
+    }
+
+    // Tentukan jenis voucher (diskon atau rupiah)
+    $voucherType = ($discount_amount > 100) ? 'rupiah' : 'diskon';
+?>
+    <tr>
+        <td><?= $i++; ?></td>
+        <td><?= htmlspecialchars($code); ?></td>
+        <td>
+            <?php if ($is_free == 1): ?>
+                0
+            <?php elseif ($voucherType == 'diskon'): ?>
+                <?= htmlspecialchars($discount_amount) . '%' ?>
+            <?php else: ?>
+                <?= 'Rp ' . number_format($discount_amount, 0, ',', '.') ?>
+            <?php endif; ?>
+        </td>
+        <td><?= htmlspecialchars($status_used); ?></td>
+        <td><?= htmlspecialchars($isFreeDisplay); ?></td>
+        <td><?= htmlspecialchars($oneTimeUse); ?></td>
+        <td>
+            <script>
+                // Mengonversi waktu UTC ke waktu lokal untuk created_at
+                var createdAtUTC = '<?= $created_at; ?>';
+                var createdAtLocal = new Date(createdAtUTC + 'Z').toLocaleString('id-ID', { 
+                    year: 'numeric', 
+                    month: '2-digit', 
+                    day: '2-digit', 
+                    hour: '2-digit', 
+                    minute: '2-digit', 
+                    second: '2-digit', 
+                    hour12: false // untuk format 24 jam
+                });
+
+                // Menghapus bagian zona waktu dan mengganti '/' dengan '-'
+                createdAtLocal = createdAtLocal.replace(/ GMT.*$/, ''); // Menghapus bagian GMT
+                createdAtLocal = createdAtLocal.replace(/\//g, '-'); // Mengganti '/' dengan '-'
+                document.write(createdAtLocal);
+            </script>
+        </td>
+        <td><?= !empty($used_at) ? htmlspecialchars(date('d-m-Y H:i:s', strtotime($used_at))) : '-'; ?></td>
+        <td><input type="checkbox" name="delete[]" value="<?= htmlspecialchars($id); ?>"></td>
+    </tr>
+<?php
+}
+?>
                                         </tbody>
                                     </table>
                                 </div>
