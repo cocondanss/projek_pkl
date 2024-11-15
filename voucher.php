a9d9888d6c3ccc98ee27bf80962d7e6e2ad5ad75
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-15 13:07:18 +0700

    hilangin tabel gratis

--- voucher.php
+++ voucher.php
@@ -248,6 +248,7 @@ function useVoucher($code) {
                                                 <th>Code</th>
                                                 <th>Diskon</th>
                                                 <th>Status</th>
+                                                <!-- <th>Gratis</th> -->
                                                 <th>Sekali Pakai</th>
                                                 <th>Tanggal Dibuat</th>
                                                 <th>Tanggal Digunakan</th>
@@ -301,7 +302,7 @@ function useVoucher($code) {
                                                         <?php endif; ?>
                                                     </td>
                                                     <td><?= htmlspecialchars($status_used); ?></td>
-                                                    <td><?= htmlspecialchars($isFreeDisplay); ?></td>
+                                                    <!-- <td><?= htmlspecialchars($isFreeDisplay); ?></td> -->
                                                     <td><?= htmlspecialchars($oneTimeUse); ?></td>
                                                     <td>
                                                         <script>
