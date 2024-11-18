c4403f3af13e31b2877f76e378d612073804633f
hawa <hawanf1234@gmail.com>
2024-11-18 14:15:25 +0700

    coba..

--- listproduct.php
+++ listproduct.php
@@ -474,7 +474,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
     }
 
     // Jika harga adalah Rp 0, langsung arahkan ke halaman transaksi berhasil
-    if (price <= 0.00) {
+    if (price <= 1) {
     console.log('Harga produk adalah Rp 0, mengarahkan ke transberhasil.php');
     const orderId = 'TRX-' + Date.now(); // Simulasi ID transaksi
     sessionStorage.setItem('successful_transaction', JSON.stringify({
