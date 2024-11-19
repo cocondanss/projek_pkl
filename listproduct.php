e872c2bc7d0ec7384e2a9ecbaed703a9371144f0
hawa <hawanf1234@gmail.com>
2024-11-19 13:07:24 +0700

    n

--- listproduct.php
+++ listproduct.php
@@ -44,6 +44,8 @@ function applyVoucher($voucherCode, $price) {
 // Inisialisasi variabel untuk sistem voucher
 $voucherMessages = [];
 $voucherCode = '';
+$originalPrice = 0; // Inisialisasi harga asli
+$discountedPrice = 0; // Inisialisasi harga diskon
 
 // Proses pengecekan voucher saat ada POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
@@ -92,7 +94,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
     }
 }
 
-
 // Ambil data produk yang visible
 $produk = mysqli_query($conn, "SELECT * FROM products WHERE visible = 1");
 if (!$produk) {
@@ -128,25 +129,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
                 <div class="row">
                     <div class="product-list" style="background: none;" id="product-list">
                     <?php foreach ($produk as $item): 
-                            $originalPrice = $item['price'];
-                            $discountedPrice = applyVoucher($voucherCode, $originalPrice);
-                            ?>
-                                <div class="product" data-product-id="<?php echo $item['id']; ?>" style="">
-                                    <div class="card-body"> 
-                                        <h2><?php echo htmlspecialchars($item['name']); ?></h2>
-                                            <div class="price-container">
-                                                <?php if ($discountedPrice < $originalPrice): ?>
-                                                    <p class="original-price">Rp <span><?php echo number_format($originalPrice, 0, ',', '.'); ?>,00</span></p>
-                                                    <p class="discounted-price">Rp <span><?php echo number_format($discountedPrice, 0, ',', '.'); ?>,00</span></p>
-                                                <?php else: ?>
-                                                    <p>Rp <span><?php echo number_format($originalPrice, 0, ',', '.'); ?></span></p>
-                                                    <?php endif; ?>
-                                                </div>
-                                            <p><?php echo htmlspecialchars($item['description']); ?></p>
-                                            <button onclick="showPaymentModal(<?php echo $item['id']; ?>, '<?php echo htmlspecialchars($item['name']); ?>', <?php echo $discountedPrice; ?>)">Buy</button>
-                                        </div>
-                                    </div>
-                                    <?php endforeach; ?>
+    $originalPrice = $item['price'];
+    // Terapkan voucher untuk mendapatkan harga diskon
+    $discountedPrice = applyVoucher($voucherCode, $originalPrice);
+?>
+    <div class="product" data-product-id="<?php echo $item['id']; ?>" style="">
+        <div class="card-body"> 
+            <h2><?php echo htmlspecialchars($item['name']); ?></h2>
+            <div class="price-container">
+                <?php if ($discountedPrice < $originalPrice): ?>
+                    <p class="original-price">Rp <span><?php echo number_format($originalPrice, 0, ',', '.'); ?>,00</span></p>
+                    <p class="discounted-price">Rp <span><?php echo number_format($discountedPrice, 0, ',', '.'); ?>,00</span></p>
+                <?php else: ?>
+                    <p>Rp <span><?php echo number_format($originalPrice, 0, ',', '.'); ?>,00</span></p>
+                <?php endif; ?>
+            </div>
+            <p><?php echo htmlspecialchars($item['description']); ?></p>
+            <button onclick="showPaymentModal(<?php echo $item['id']; ?>, '<?php echo htmlspecialchars($item['name']); ?>', <?php echo number_format($discountedPrice, 0, '', ''); ?>)">Buy</button>
+        </div>
+    </div>
+<?php endforeach; ?>
                                         <div class="voucher-form">
                                             <div id="voucher-message-container">
                                                 <?php
