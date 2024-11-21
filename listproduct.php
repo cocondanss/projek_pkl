cd409ddbdf931f839ea4faa14dc1daea855b061d
hawa <hawanf1234@gmail.com>
2024-11-21 08:43:59 +0700

    .,h

--- listproduct.php
+++ listproduct.php
@@ -131,7 +131,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
                     <div class="product-list" style="background: none;" id="product-list">
                     <?php foreach ($produk as $item): 
                         $originalPrice = $item['price'];
-                        $discountedPrice = isset($_SESSION['discountedPrice']) ? $_SESSION['discountedPrice'] : applyVoucher($voucherCode, $originalPrice);             
+                        $discountedPrice = applyVoucher($voucherCode, $originalPrice);             
                     ?>
                         <div class="product" data-product-id="<?php echo $item['id']; ?>" style="">
                             <div class="card-body"> 
