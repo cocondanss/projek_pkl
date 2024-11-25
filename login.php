2c75fb1ea1bc69b94cbe448bc9f47c4c62634444
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-25 16:18:21 +0700

    block

--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if(isset($_POST['kembali'])){
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Login</title>
-    <link href="css/style.css" rel="stylesheet" />
+    <!-- <link href="css/style.css" rel="stylesheet" /> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     <style>
         body {
