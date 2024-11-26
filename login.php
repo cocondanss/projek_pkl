63521a7de1a25286309cb2efe7aae2d88892f2f2
cocondanss <ridhoauliaakbar1@gmail.com>
2024-11-26 11:07:08 +0700

    undo

--- login.php
+++ login.php
@@ -86,10 +86,6 @@ if(isset($_POST['kembali'])){
             position: relative;
             bottom: 33px;
         }
-
-        .px-3 {
-            padding-right: 50px;
-        }
     </style>
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
