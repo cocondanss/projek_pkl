f4d9a791854ec865d1a273edf4ff249a8efef4bc
hawa <hawanf1234@gmail.com>
2024-11-29 13:46:14 +0700

    d

--- listproduct.php
+++ listproduct.php
@@ -499,7 +499,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
                 }
 
                 // Jika harga kurang dari atau sama dengan Rp 0, arahkan ke transberhasil
-                if (price <= 0, discount <= 0) {
+                if (price <= 0 || discount <= 0) {
                     console.log('Price is less than or equal to 0, redirecting to transberhasil.php');
                     window.location.href = 'transberhasil.php';
                     return; // Hentikan eksekusi lebih lanjut
