2b095a8d4877f30b74757a8419b3d9b4213254f0
hawa <hawanf1234@gmail.com>
2024-12-19 11:44:50 +0700

    2

--- listproduct.php
+++ listproduct.php
@@ -10,6 +10,9 @@ require 'function.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+$backgroundType = getSetting('background_type');
+$backgroundFile = getSetting('background_file');
 /**
  * Fungsi untuk menerapkan voucher pada harga produk
  * @param string $voucherCode - Kode voucher yang diinput
