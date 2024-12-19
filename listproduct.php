fcd019f1fcd02ac929998f4549cc20cfa188c0d8
hawa <hawanf1234@gmail.com>
2024-12-19 10:06:55 +0700

    undo

--- listproduct.php
+++ listproduct.php
@@ -142,13 +142,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
         </div>
         <div class="product-container">
             <div class="row">
-            <div class="product product-<?php echo $item['id']; ?>" style="background-color: #ff5733;" data-product-id="<?php echo $item['id']; ?>">
+                <div class="product-list" style="background: none;" id="product-list">
                 <?php foreach ($produk as $item): 
                     $originalPrice = $item['price'];
                     // Hitung harga diskon berdasarkan voucher yang ada
                     $discountedPrice = applyVoucher($voucherCode, $originalPrice);             
                 ?>
-                    <div class="product" data-product-id="<?php echo $item['id']; ?>" style="">
+                    <div class="product product-<?php echo $item['id']; ?>" data-product-id="<?php echo $item['id']; ?>" style="">
                         <div class="card-body"> 
                             <h2><?php echo htmlspecialchars($item['name']); ?></h2>
                             <div class="price-container">
@@ -181,7 +181,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['voucher_code'])) {
                 </div>
             </div>
         </div>
-
     </div>
 </div>
     <div class="modal fade" id="virtualKeyboardModal" tabindex="-1" aria-hidden="true">
