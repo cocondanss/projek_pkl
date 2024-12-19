0a31e73c5cbe10efc13841f5b52e2179cd06be4f
hawa <hawanf1234@gmail.com>
2024-12-19 15:17:13 +0700

    undo

--- listproduct.php
+++ listproduct.php
@@ -156,7 +156,7 @@ $background_file = getSetting('background_file');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="css/style3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- <link href="css/styleLP2.css" rel="stylesheet"/> -->
+    <link href="css/styleLP2.css" rel="stylesheet"/>
 </head>
 <body class="<?php echo $background_type == 'image' ? 'background-image' : 'background-video'; ?>">
     <?php if ($background_type == 'video'): ?>
