77ad5ed92f5e05be2aedc1f68807cd2ad1acde2c
hawa <hawanf1234@gmail.com>
2024-12-26 12:48:51 +0700

    tesLagi

--- listproduct.php
+++ listproduct.php
@@ -148,7 +148,7 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="css/style4.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styleLP2.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/styleLP2.css">
 </head>
 <body>
 <?php if ($backgroundType == 'video'): ?>
