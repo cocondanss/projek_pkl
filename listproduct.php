2502993cce5d19e67ceede6f56e768b825758473
hawa <hawanf1234@gmail.com>
2024-12-30 08:30:32 +0700

    5

--- listproduct.php
+++ listproduct.php
@@ -147,7 +147,7 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="css/style4.css">
-    <!-- <link rel="stylesheet" href="css/styleLP2.css"> -->
+    <link rel="stylesheet" href="css/styleLP2.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
