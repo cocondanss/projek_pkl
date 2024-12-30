2f122b15ed21a287fb86870984e7383c26364cb8
hawa <hawanf1234@gmail.com>
2024-12-30 15:18:43 +0700

    undo

--- listproduct.php
+++ listproduct.php
@@ -129,11 +129,11 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Product</title>
+    <link rel="stylesheet" href="css/style4.css">
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styleLP2.css">
-    <link rel="stylesheet" href="css/style4.css">
     <style>
     body {
     <?php if ($backgroundType == 'image'): ?>
