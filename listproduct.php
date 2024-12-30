bea18e81a1a012feec06da04c54f47dc41c780e7
hawa <hawanf1234@gmail.com>
2024-12-30 10:37:03 +0700

    3

--- listproduct.php
+++ listproduct.php
@@ -129,7 +129,11 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Product</title>
-    <!-- <link rel="stylesheet" href="css/styleLP2.css"> -->
+    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="css/style4.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styleLP2.css">
     <style>
     body {
     <?php if ($backgroundType == 'image'): ?>
@@ -144,11 +148,6 @@ $backgroundType = file_get_contents('config/background_type.txt');
     margin: 0; /* Remove default margin */
     }
     </style>
-    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="css/style4.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styleLP2.css">
 </head>
 <body>
 <?php if ($backgroundType == 'video'): ?>
