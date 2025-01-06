37bf00dceb9c1f2764502bf3840cbc7a40bb5864
hawa <hawanf1234@gmail.com>
2025-01-06 10:32:33 +0700

    la

--- listproduct.php
+++ listproduct.php
@@ -126,7 +126,7 @@ $backgroundType = file_get_contents('config/background_type.txt');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Product</title>
     <link rel="stylesheet" href="css/style4.css">
