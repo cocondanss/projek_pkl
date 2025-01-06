4c692a47fbe7ffc3f36d3b631f91f8b1e0a27da7
hawa <hawanf1234@gmail.com>
2025-01-06 10:41:38 +0700

    ?

--- listproduct.php
+++ listproduct.php
@@ -133,7 +133,7 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styleLP2.css?v=1.1">
+    <link rel="stylesheet" href="css/styleLP2.css?v=1.4">
     <style>
     body {
     <?php if ($backgroundType == 'image'): ?>
