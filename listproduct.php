2ca1317f006de6898716d118e86708766e2c2555
hawa <hawanf1234@gmail.com>
2025-01-06 10:09:41 +0700

    cik

--- listproduct.php
+++ listproduct.php
@@ -133,7 +133,7 @@ $backgroundType = file_get_contents('config/background_type.txt');
     <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styleLP2.css">
+    <link rel="stylesheet" href="css/styleLP2.css?v=1.0">
     <style>
     body {
     <?php if ($backgroundType == 'image'): ?>
@@ -142,10 +142,8 @@ $backgroundType = file_get_contents('config/background_type.txt');
         background-repeat: no-repeat !important;
         background-position: center center !important;
     <?php elseif ($backgroundType == 'video'): ?>
-        background: none !important;
+        /* Video background styles */
     <?php endif; ?>
-    height: 100vh; /* Ensure the body takes full height */
-    margin: 0; /* Remove default margin */
     }
     </style>
 </head>
